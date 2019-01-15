0b957e1fdd8b19c3505d856baeab0248f8db02f6
hong.chen <hong.chen@ms-ht.com>
2019-01-15 14:13:46 +0800

    燃气缴费键盘遮挡编辑框

---
+++ minshengbao/app/src/main/java/com/msht/minshengbao/functionActivity/GasService/GasOrdinaryRecordActivity.java
@@ -0,0 +1,13 @@
+package com.msht.minshengbao.functionActivity.GasService;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GasOrdinaryRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gas_ordinary_record);
+    }
+}
