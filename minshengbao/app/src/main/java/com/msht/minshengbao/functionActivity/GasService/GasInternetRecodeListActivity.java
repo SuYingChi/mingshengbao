4244884eab8b0bb2da6f1b600ffed8e69a9f2721
hong.chen <hong.chen@ms-ht.com>
2019-01-31 17:43:42 +0800

    NB表充值记录

---
+++ minshengbao/app/src/main/java/com/msht/minshengbao/functionActivity/GasService/GasInternetRecodeListActivity.java
@@ -0,0 +1,13 @@
+package com.msht.minshengbao.functionActivity.GasService;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GasInternetRecodeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gas_internet_recode_list);
+    }
+}
