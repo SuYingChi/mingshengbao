8ca4e49225be62ac36db545152c50524e9bf0e2b
苏英驰 <yingchi.su@ms-ht.com>
2019-05-07 13:06:14 +0800

    界面优化

--- minshengbao/app/src/main/java/com/msht/minshengbao/androidShop/adapter/HotSearchAdapter.java
+++ minshengbao/app/src/main/java/com/msht/minshengbao/androidShop/adapter/HotSearchAdapter.java
@@ -19,22 +19,22 @@ public class HotSearchAdapter extends MyHaveHeadViewRecyclerAdapter<String> {
     private LinearLayout.LayoutParams mLayoutParams;
     public HotSearchAdapter(Context context) {
     super(context, R.layout.item_text);
-    int screenWidth = DimenUtil.getScreenWidth();
+   /* int screenWidth = DimenUtil.getScreenWidth();
     int totalPadding = DimenUtil.dip2px( 20);
     int width = (screenWidth - totalPadding) / 5;
     mLayoutParams = new LinearLayout.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT);
-    mLayoutParams.gravity= Gravity.CENTER_HORIZONTAL;
+    mLayoutParams.gravity= Gravity.CENTER_HORIZONTAL;*/
 }
 
 
     @Override
     public void convert(RecyclerHolder holder, String s, final int position) {
         //动态设置外层的布局参数，来调整item间隔和大小
-        holder.getConvertView().setLayoutParams(mLayoutParams);
+      /*  holder.getConvertView().setLayoutParams(mLayoutParams);*/
         TextView tv = holder.getView(R.id.tv);
-        tv.setPadding(0,context.getResources().getDimensionPixelSize(R.dimen.margin_Modules),0,context.getResources().getDimensionPixelSize(R.dimen.margin_Modules));
-        tv.setText(s);
+  /*      tv.setPadding(0,context.getResources().getDimensionPixelSize(R.dimen.margin_Modules),0,context.getResources().getDimensionPixelSize(R.dimen.margin_Modules));*/
         tv.setBackgroundDrawable(null);
+        tv.setText(s);
         holder.getConvertView().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
